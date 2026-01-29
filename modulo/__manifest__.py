49a012e9327cbff9fd7652a049a334f1a31f8146
CarlosBadilloC <carlos.badillo@sellside.cl>
2026-01-29 11:27:20 -0300

    agente ia prueba 3

--- modulo/__manifest__.py
+++ modulo/__manifest__.py
@@ -3,8 +3,10 @@
 
     'summary': "Short (1 phrase/line) summary of the module's purpose",
 
-    'description': """
-Long description of module's purpose
+   'description': """
+    AI Agents for Odoo
+    
+    This module provides a framework for using AI agents in Odoo to automate tasks and provide assistance to users.
     """,
 
     'author': "My Company",
@@ -15,19 +17,26 @@ Long description of module's purpose
     # for the full list
     'category': 'Uncategorized',
     'version': '0.1',
+    'license': 'LGPL-3',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'mail', 'web'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
+        'views/ai.xml',
+        'views/task.xml',
         'views/templates.xml',
+        # Data
+        'data/cronjobs.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
     ],
+    "application": True,
 }
 
--- modulo/models/__init__.py
+++ modulo/models/__init__.py
@@ -1,1 +1,3 @@
-from . import models
+from . import ai
+from . import discuss_channel
+from . import task
