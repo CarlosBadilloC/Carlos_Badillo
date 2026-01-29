79997a9c66e049b1a025e1f7c04973a0cc98b154
CarlosBadilloC <carlos.badillo@sellside.cl>
2026-01-29 17:43:52 -0300

    creacion de agente

--- modulo/__manifest__.py
+++ modulo/__manifest__.py
@@ -22,7 +22,7 @@ Long description of module's purpose
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'data/ai_actions.xml',
+         'data/ai_actions.xml',
         'data/ai_agent.xml',
         'data/ai_agent_source.xml',
         'views/views.xml',
