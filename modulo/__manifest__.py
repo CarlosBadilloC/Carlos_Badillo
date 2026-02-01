8d34784c8f1235a7d26827f1f170581a46d08465
CarlosBadilloC <carlos.badillo@sellside.cl>
2026-02-01 14:09:46 -0300

    añade funcionalidades de crm para el agente

--- modulo/__manifest__.py
+++ modulo/__manifest__.py
@@ -17,7 +17,7 @@ Long description of module's purpose
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','mail','product','stock','ai'],
+    'depends': ['base','mail','product','stock','ai','crm'],
 
     # always loaded
     'data': [
